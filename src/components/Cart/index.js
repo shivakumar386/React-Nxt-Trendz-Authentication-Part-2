51be7d6f14bf1a9cffa0c64169d088816dbb2205
shivakumar386 <shivakumarkantem528@gmail.com>
2022-09-30 10:27:17 +0000

    ReactNxtTrendzAuthenticationPart2

---
+++ src/components/Cart/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Cart = () => (
+  <div className="cart-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="cart-images"
+    />
+  </div>
+)
+
+export default Cart
